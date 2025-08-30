013756596db8254121ea4eba1d183904e2d926b0
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2025-08-30 19:48:17 +0000

    Initial commit with squashed history

---
+++ README.md
@@ -0,0 +1,8 @@
+# My .dotfiles
+
+## Installation
+```sh
+git clone git@github.com:bacali95/.dotfiles.git
+cd .dotfiles
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+runOrFail() {
+    sh -c "$1 $2"
+    EXIT_CODE=$?
+    [ $EXIT_CODE -ne 0 ] && exit $EXIT_CODE
+}
+
+echo "Installing useful tools..."
+runOrFail "sudo apt update"
+runOrFail "sudo apt install -y zsh git curl wget"
+
+echo "Copying config files..."
+if [ -d "$HOME/.dotfiles" ]; then
+    runOrFail "rm -rf $HOME/.dotfiles"
+fi
+
+runOrFail "cp -r ./config $HOME/.dotfiles"
+
+OH_MY_ZSH_DIRECTORY="$HOME/.oh-my-zsh"
+
+echo "Installing Oh-My-Zsh"
+if [ ! -d "$OH_MY_ZSH_DIRECTORY" ]; then
+    runOrFail "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "--unattended"
+fi
+
+echo "Installing Powerlevel10k custom theme..."
+if [ -d "$OH_MY_ZSH_DIRECTORY/custom/themes/powerlevel10k" ]; then
+    runOrFail "rm -rf $OH_MY_ZSH_DIRECTORY/custom/themes/powerlevel10k"
+fi
+runOrFail "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $OH_MY_ZSH_DIRECTORY/custom/themes/powerlevel10k"
+
+echo "Installing Oh-My-Zsh Custom Plugins..."
+for pluginRepo in "zsh-users/zsh-autosuggestions" "zsh-users/zsh-completions" "zsh-users/zsh-syntax-highlighting" "bacali95/cf-submit-completion" "jscutlery/nx-completion"
+do
+    pluginPath="$OH_MY_ZSH_DIRECTORY/custom/plugins/${pluginRepo##*/}"
+    runOrFail "rm -rf $pluginPath"
+    runOrFail "git clone https://github.com/$pluginRepo $pluginPath"
+done
+
+echo "Creating symbolic links for config files..."
+for filename in "gitconfig" "zshrc" "ssh" "p10k.zsh"
+do
+    rm -rf $HOME/.$filename
+    ln -sf $HOME/.dotfiles/$filename $HOME/.$filename
+done
+
+echo "Installing Volta..."
+runOrFail "$(curl https://get.volta.sh | bash)"
+runOrFail "mkdir -p $OH_MY_ZSH_DIRECTORY/custom/plugins/volta"
+runOrFail "$HOME/.volta/bin/volta completions zsh -f -o $OH_MY_ZSH_DIRECTORY/custom/plugins/volta/_volta"
+
+exit 0
